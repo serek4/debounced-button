f006c52064abec0461095fca0611361f0ebc270c
serek4 <jacezarem@gmail.com>
2017-12-06 01:16:13 +0100

    repeat button presses while button is held down

--- debounced-button.ino
+++ debounced-button.ino
@@ -3,22 +3,38 @@
 
 boolean led = LOW;
 byte button_lock = 0;
+unsigned long pressTime = 0;
 
 void setup() {
 	pinMode(BUTTON, INPUT_PULLUP);
 	pinMode(LED, OUTPUT);
 	digitalWrite(LED, LOW);
+	Serial.begin(115200);
 	delay(10);
 }
 
 void loop() {
-	if (!button_lock && digitalRead(BUTTON) == LOW) {
+	if (button(BUTTON)) {
+		led = !led;
+	}
+	digitalWrite(LED, led);
+}
+
+boolean button(int button) {
+	boolean press = false;
+	if (!button_lock && digitalRead(button) == LOW) {
 		// button pressed
 		button_lock = 1;
-		led = !led;
-	} else if (button_lock && digitalRead(BUTTON) == HIGH) {
+		press = true;
+		pressTime = millis();
+		Serial.print("press: ");
+		Serial.println(pressTime);
+	} else if (millis() - pressTime > 1000 && digitalRead(button) == LOW) {
+		// repeat button presses
+		button_lock = 0;
+	} else if (button_lock && digitalRead(button) == HIGH) {
 		// debounce
 		button_lock++;
 	}
-	digitalWrite(LED, led);
+	return press;
 }

1b279b5e06716c6d703535548219321331ab32a2
serek4 <jacezarem@gmail.com>
2017-12-06 10:55:14 +0100

    second button activated on release

--- debounced-button.ino
+++ debounced-button.ino
@@ -1,12 +1,16 @@
 #define LED 13
-#define BUTTON 8
+#define BUTTON1 8
+#define BUTTON2 9
 
 boolean led = LOW;
-byte button_lock = 0;
-unsigned long pressTime = 0;
+byte button1_lock = 0;
+byte button2_lock = 0;
+unsigned long pressTime1 = 0;
+unsigned long pressTime2 = 0;
 
 void setup() {
-	pinMode(BUTTON, INPUT_PULLUP);
+	pinMode(BUTTON1, INPUT_PULLUP);
+	pinMode(BUTTON2, INPUT_PULLUP);
 	pinMode(LED, OUTPUT);
 	digitalWrite(LED, LOW);
 	Serial.begin(115200);
@@ -14,27 +18,56 @@ void setup() {
 }
 
 void loop() {
-	if (button(BUTTON)) {
+	if (press(BUTTON1)) {
+		led = !led;
+	}
+	if (release(BUTTON2)) {
 		led = !led;
 	}
 	digitalWrite(LED, led);
 }
 
-boolean button(int button) {
-	boolean press = false;
-	if (!button_lock && digitalRead(button) == LOW) {
+boolean press(int button) {
+	boolean activate = false;
+	if (!button1_lock && digitalRead(button) == LOW) {
 		// button pressed
-		button_lock = 1;
-		press = true;
-		pressTime = millis();
+		button1_lock = 1;
+		activate = true;
+		pressTime1 = millis();
 		Serial.print("press: ");
-		Serial.println(pressTime);
-	} else if (millis() - pressTime > 1000 && digitalRead(button) == LOW) {
+		Serial.println(pressTime1);
+	} else if (button1_lock && millis() - pressTime1 > 1000 && digitalRead(button) == LOW) {
 		// repeat button presses
-		button_lock = 0;
-	} else if (button_lock && digitalRead(button) == HIGH) {
+		button1_lock = 0;
+	} else if (button1_lock && digitalRead(button) == HIGH) {
 		// debounce
-		button_lock++;
+		button1_lock++;
 	}
-	return press;
+	return activate;
+}
+
+boolean release(int button) {
+	boolean activate = false;
+	if (!button2_lock && digitalRead(button) == LOW) {
+		// button pressed
+		button2_lock = 1;
+		pressTime2 = millis();
+	} else if (button2_lock && millis() - pressTime2 > 2000 && digitalRead(button) == LOW) {
+		// repeat button presses(releases)
+		button2_lock = 1;
+		activate = true;
+		pressTime2 = millis();
+		Serial.print("release: ");
+		Serial.println(pressTime2);
+	} else if (button2_lock && digitalRead(button) == HIGH) {
+		// debounce
+		button2_lock++;
+		if (!button2_lock) {
+			// realase
+			activate = true;
+			Serial.print("release: ");
+			Serial.println(millis());
+		}
+	}
+	return activate;
 }
